12fb1d8b198f3bfa1f1e7d67526b6dc6a6d7d41d
felicha1120 <44919538+felicha1120@users.noreply.github.com>
2021-03-23 16:53:32 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,11 +1,25 @@
 package Modul1;
-import java.util.Scanner;
+
+import java.lang.*;
 public class Person {
-	 	String name;
-		char gender;
-		int age;
-		String dateOfBirth;
-		double height;
-		int weight;
-		String address;
-}
+ public String name;
+ public char gender;
+ public int age;
+ public String dateOfBirth;
+ public float height;
+ public float weight;
+ public String address;
+
+
+ 	public void cetakBiodata(String name, char gender,String address){
+ 		System.out.println("nama "+name+" ,\n jenis kelamin "+gender+",\n Alamat "+address);
+ }
+
+ 	public void cetakFisik(int age,String dateOfBirth,float height, float weight){
+ 		System.out.println("Umur : "+age);
+ 		System.out.println("Tanggal Lahir : "+dateOfBirth);
+ 		System.out.println("Tinggi : "+height);
+ 		System.out.println("Berat : "+weight);
+ }
+
+} 
